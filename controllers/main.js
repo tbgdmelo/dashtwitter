fed833826dd5bba2dc6e4b01079978c27b9f446c
Thiago Melo <tbgdmelo@gmail.com>
2021-12-08 12:00:34 -0400

    novas rotas e template das paginas

--- controllers/main.js
+++ controllers/main.js
@@ -8,4 +8,16 @@ async function sobre(req,res){
     res.render("main/sobre",{titulo:"Sobre"});
 }
 
-module.exports = { index, sobre };
+async function nuvem(req,res){
+    res.render("main/nuvem",{titulo:"Nuvem de Palavras"});
+}
+
+async function analise(req,res){
+    res.render("main/analise",{titulo:"Análise de Tweet"});
+}
+
+async function graficos(req,res){
+    res.render("main/graficos",{titulo:"Gráficos"});
+}
+
+module.exports = { index, sobre, nuvem, analise, graficos };
--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,14 @@ var express = require('express');
 const mainController = require("../controllers/main");
 var router = express.Router();
 
-
-/* GET home page. */
 router.get("/", mainController.index);
 
 router.get("/sobre", mainController.sobre);
 
+router.get("/analise", mainController.analise);
+
+router.get("/nuvem", mainController.nuvem);
+
+//router.get("/graficos/:id", mainController.sobre);
 
 module.exports = router;
