ab49b3b87f1184d81467d5e25f7759d5f341a4f7
Thiago Melo <tbgdmelo@gmail.com>
2021-12-08 19:20:26 -0400

    app

--- app.js
+++ app.js
@@ -57,6 +57,6 @@ app.use(function(err, req, res, next) {
 });*/
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 module.exports = app;
