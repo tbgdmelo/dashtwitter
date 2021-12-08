701889f3284d085ead9edf544fd0dd381bb8a6d8
Thiago Melo <tbgdmelo@gmail.com>
2021-12-08 19:14:34 -0400

    app

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });*/
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 
 module.exports = app;
